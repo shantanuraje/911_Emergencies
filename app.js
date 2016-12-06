f5c06fddd0d3cc77567b6584dc6e7fb2712cd6b0
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-12-05 22:51:04 -0500

    added leaflet mapping lib with openstreetmap tiling, center at US, full page

--- app.js
+++ app.js
@@ -11,16 +11,11 @@ function Emergency(latitude,longitude,description,zip,title,timeStamp,township,a
 var Emergencies=[]
 var t0 = performance.now();
 d3.csv("/dataset/911.csv", function(data) {
-  // console.log(typeof(data));
-  // console.log(data.length);
   var t0 = performance.now();
   for (var i=0;i<data.length;i++) {
-    // console.log(data[i]);
-    //dont need dummy variable e
     var emergency = new Emergency(data[i].lat,data[i].lng,data[i].desc,data[i].zip,data[i].title,data[i].timeStamp,data[i].twp,data[i].addr,data[i].e);
     Emergencies.push(emergency);
   }
-  // console.log(Emergencies);
   var t1 = performance.now();
   console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.")
 });
