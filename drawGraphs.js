8ebcc1005372f7d2f2ba3957cd2540796f67c4c3
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-12-10 16:45:54 -0500

    svg takes width and height of viewport,added width and color (blue to red) scaling to rectangles

--- drawGraphs.js
+++ drawGraphs.js
@@ -3,18 +3,32 @@ function emergencyCountVsCategory(emergencyCategories, noOfEmergenciesPerCategor
 
   var width = 500,
       height = 500;
-
+  //get window witdh height
+  // document.documentElement.clientWidth or window.innerWidth
   //Create the SVG Viewport
   var svg = d3.select("body")
               .append("svg")
-              .attr("width",width)
-              .attr("height",height);
+              .attr("width",document.documentElement.clientWidth)
+              .attr("height",document.documentElement.clientHeight);
+
+  var widthScale = d3.scale.linear()
+                    .domain([0,d3.max(noOfEmergenciesPerCategory)])
+                    .range([0,window.innerWidth])
+
+  var colorScale = d3.scale.linear()
+                    .domain([0,d3.max(noOfEmergenciesPerCategory)])
+                    .range(["blue","red"])
+  console.log(noOfEmergenciesPerCategory.map(function (d) {
+    return widthScale(d);
+  }))
+
   var bars = svg.selectAll("rect")
                 .data(noOfEmergenciesPerCategory)
                 .enter()
                   .append("rect")
-                  .attr("width",50)
-                  .attr("height",function(d){return d/100})
-                  .attr("fill","red")
-                  .attr("x",function(d,i){return i*100})
+                  .attr("height",50)
+                  .attr("width",function(d){return widthScale(d)})
+                  .attr("fill",function(d){return colorScale(d)})
+                  .attr("y",function(d,i){return i*100})
+                  .attr("transform","translate(50,50)")
 }
